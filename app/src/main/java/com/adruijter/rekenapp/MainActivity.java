5aff6b412a9c9831bc6142b1bc849a6202c15361
Arjan de Ruijter <rra@mboutrecht.nl>
2016-01-27 15:02:02 +0100

    Rekenapp

--- app/src/main/java/com/adruijter/rekenapp/MainActivity.java
+++ app/src/main/java/com/adruijter/rekenapp/MainActivity.java
@@ -8,26 +8,53 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity
 {
     // Fields
-    private int number = 0;
+    private int numberLeft = 5;
+    private int numberRight = 10;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textLeft = (TextView) findViewById(R.id.txtNumberLeft);
+        textLeft.setText(Integer.toString(this.numberLeft));
+        TextView textRight = (TextView) findViewById(R.id.txtNumberRight);
+        textRight.setText(Integer.toString(this.numberRight));
+
     }
 
-    public void doMinusOne(View view)
+    public void doMinusOneLeft(View view)
     {
-        this.number -= 1;
-        TextView txt = (TextView) findViewById(R.id.txtNumber);
-        txt.setText(Integer.toString(this.number));
+        this.numberLeft -= 1;
+        TextView txt = (TextView) findViewById(R.id.txtNumberLeft);
+        txt.setText(Integer.toString(this.numberLeft));
     }
 
-    public void doPlusOne(View view)
+    public void doPlusOneLeft(View view)
     {
-        this.number += 1;
-        TextView txt = (TextView) findViewById(R.id.txtNumber);
-        txt.setText(Integer.toString(this.number));
+        this.numberLeft += 1;
+        TextView txt = (TextView) findViewById(R.id.txtNumberLeft);
+        txt.setText(Integer.toString(this.numberLeft));
+    }
+
+    public void doMinusOneRight(View view)
+    {
+        this.numberRight -= 1;
+        TextView txt = (TextView) findViewById(R.id.txtNumberRight);
+        txt.setText(Integer.toString(this.numberRight));
+    }
+
+    public void doPlusOneRight(View view)
+    {
+        this.numberRight += 1;
+        TextView txt = (TextView) findViewById(R.id.txtNumberRight);
+        txt.setText(Integer.toString(this.numberRight));
+    }
+
+    public void sum(View view)
+    {
+        int result = numberLeft + numberRight;
+        TextView txt = (TextView) findViewById(R.id.txtSum);
+        txt.setText(Integer.toString(result));
     }
 
 
